a5a24cb4f16ff501a9f0c853489b86051db33ab1
motchang <motchang@gmail.com>
2013-11-17 17:39:47 +0900

    mod setting about flymake with php

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -556,9 +556,9 @@
 (when (require 'flymake nil t)
   (global-set-key "\C-cd" 'flymake-display-err-menu-for-current-line)
   (set-face-background 'flymake-errline "Red")
-  ;(set-face-underline-p 'flymake-errline t)
-  (set-face-underline-p 'flymake-errline nil)
+  (set-face-underline-p 'flymake-errline t)
   ;; PHP
+  ;; PHPで flymake する時は display_errors = On じゃないとダメだよ
   (when (not (fboundp 'flymake-php-init))
     (defun flymake-php-init ()
       (let* ((temp-file (flymake-init-create-temp-buffer-copy
@@ -575,6 +575,62 @@
           (cons
            '("\(\(?:Parse error\|Fatal error\|Warning\): .*\) in \(.*\) on line \([0-9]+\)" 2 3 nil 1)
            flymake-err-line-patterns)))
+  ;; JavaScript
+  (when (not (fboundp 'flymake-javascript-init))
+    (defun flymake-javascript-init ()
+      (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                         'flymake-create-temp-inplace))
+             (local-file (file-relative-name
+                          temp-file
+                          (file-name-directory buffer-file-name))))
+        (list "/usr/local/bin/jsl" (list "-process" local-file))))
+    (setq flymake-allowed-file-name-masks
+          (append
+           flymake-allowed-file-name-masks
+           '(("\.json$" flymake-javascript-init)
+             ("\.js$" flymake-javascript-init))))
+    (setq flymake-err-line-patterns
+          (cons
+           '("\(.+\)(\([0-9]+\)): \(?:lint \)?\(\(?:Warning\|SyntaxError\):.+\)" 1 2 nil 3)
+           flymake-err-line-patterns)))
+  ;; Ruby
+  (when (not (fboundp 'flymake-ruby-init))
+    (defun flymake-ruby-init ()
+      (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                         'flymake-create-temp-inplace))
+             (local-file (file-relative-name
+                          temp-file
+                          (file-name-directory buffer-file-name))))
+        '("ruby" '("-c" local-file)))))
+  (add-hook 'php-mode-hook
+            '(lambda () (flymake-mode t)))
+  (add-hook 'js-mode-hook
+            (lambda () (flymake-mode t)))
+  (add-hook 'ruby-mode-hook
+            (lambda () (flymake-mode t))))
+
+;; (when (require 'flymake nil t)
+;;   (global-set-key "\C-cd" 'flymake-display-err-menu-for-current-line)
+;;   (set-face-background 'flymake-errline "Red")
+;;   ;(set-face-underline-p 'flymake-errline t)
+;;   (set-face-underline-p 'flymake-errline nil)
+;;   ;; PHP
+;;   (when (not (fboundp 'flymake-php-init))
+;;     (defun flymake-php-init ()
+;;       (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                          'flymake-create-temp-inplace))
+;;              (local-file (file-relative-name
+;;                           temp-file
+;;                           (file-name-directory buffer-file-name))))
+;;         (list "php" (list "-f" local-file "-l"))))
+;;     (setq flymake-allowed-file-name-masks
+;;           (append
+;;            flymake-allowed-file-name-masks
+;;            '(("\.php[345]?$" flymake-php-init))))
+;;     (setq flymake-err-line-patterns
+;;           (cons
+;;            '("\(\(?:Parse error\|Fatal error\|Warning\): .*\) in \(.*\) on line \([0-9]+\)" 2 3 nil 1)
+;;            flymake-err-line-patterns)))
 ;;   ;; JavaScript
 ;;   (when (not (fboundp 'flymake-javascript-init))
 ;;     (defun flymake-javascript-init ()
@@ -602,10 +658,10 @@
 ;;                           temp-file
 ;;                           (file-name-directory buffer-file-name))))
 ;;         '("ruby" '("-c" local-file)))))
-  ) ; END OF when require 'flymake nil t
+  ;; ) ; END OF when require 'flymake nil t
 
-(add-hook 'php-mode-hook
-	  '(lambda () (flymake-mode t)))
+;; (add-hook 'php-mode-hook
+;; 	  '(lambda () (flymake-mode t)))
 ;;   (add-hook 'js-mode-hook
 ;;             '(lambda () (flymake-mode t)))
 ;;   (add-hook 'ruby-mode-hook
