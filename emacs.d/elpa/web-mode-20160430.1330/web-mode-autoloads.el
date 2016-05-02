b246f8f1ab96b37aa5a622085b615350114cef14
motchang <motchang@gmail.com>
2016-05-02 15:45:09 +0900

    update web-mode

--- emacs.d/elpa/web-mode-20160430.1330/web-mode-autoloads.el
+++ emacs.d/elpa/web-mode-20160430.1330/web-mode-autoloads.el
@@ -3,13 +3,18 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "web-mode" "web-mode.el" (21713 43733 0 0))
+;;;### (autoloads nil "web-mode" "web-mode.el" (22310 62253 0 0))
 ;;; Generated autoloads from web-mode.el
 
 (autoload 'web-mode "web-mode" "\
 Major mode for editing web templates.
 
 \(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("web-mode-pkg.el") (22310 62253 272341
+;;;;;;  0))
 
 ;;;***
 
