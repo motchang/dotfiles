dec6b2e343da6009966b6c5bac4b5751f71c81ea
motchang <motchang@gmail.com>
2016-05-21 16:54:51 +0900

    add some packages

---
+++ emacs.d/elpa/ac-R-0.2/ac-R-autoloads.el
@@ -0,0 +1,15 @@
+;;; ac-R-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ac-R.el") (22334 46148 330556 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ac-R-autoloads.el ends here
---
+++ emacs.d/elpa/e2wm-term-20141009.608/e2wm-term-pkg.el
@@ -0,0 +1,1 @@
+(define-package "e2wm-term" "20141009.608" "Perspective of e2wm.el for work in terminal" '((e2wm "1.2") (log4e "0.2.0") (yaxception "0.3.2")) :url "https://github.com/aki2o/e2wm-term" :keywords '("tools" "window manager"))
