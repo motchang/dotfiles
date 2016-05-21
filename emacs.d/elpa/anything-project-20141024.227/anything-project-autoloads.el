7290118cc2c5a37d41f889844269527b27685fd5
motchang <motchang@gmail.com>
2016-05-21 16:51:32 +0900

    remove anything

--- emacs.d/elpa/anything-project-20141024.227/anything-project-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; anything-project-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("anything-project.el") (21750 53655 493480
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; anything-project-autoloads.el ends here
