9790100c87a69dbedcbeeb6a74d3efec33efe773
motchang <motchang@gmail.com>
2016-05-21 16:54:51 +0900

    add some packages

---
+++ emacs.d/elpa/ac-R-0.2/ac-R-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ac-R" "0.2" "Autocompletion routines for R" 'nil)
---
+++ emacs.d/elpa/e2wm-sww-20140524.158/e2wm-sww-pkg.el
@@ -0,0 +1,1 @@
+(define-package "e2wm-sww" "20140524.158" "Plugin of e2wm.el to switch plugin quickly" '((e2wm "1.2")) :url "https://github.com/aki2o/e2wm-sww" :keywords '("tools" "window manager"))
