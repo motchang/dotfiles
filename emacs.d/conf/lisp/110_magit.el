0e13d0899421f58b3a874f70462cdbad4d57b839
motchang <motchang@gmail.com>
2018-07-31 16:14:57 +0900

    Add git-link

--- emacs.d/conf/lisp/110_magit.el
+++ emacs.d/conf/lisp/110_magit.el
@@ -12,3 +12,8 @@
 ;; http://qiita.com/alpha22jp/items/01e614474e7dbfd78305
 (set-default-coding-systems 'utf-8)
 (prefer-coding-system 'utf-8)
+
+;; -----------------------------------------------------------------------------
+;; git-link
+(when (require 'git-link nil t)
+  (setq git-link-open-in-browser t))
