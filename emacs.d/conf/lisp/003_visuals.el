7330d62cacf33a9d157a49de20575143c58c2a0e
motchang <motchang@gmail.com>
2018-08-13 13:31:58 +0900

    Cleanup visualis configuration

--- emacs.d/conf/lisp/003_visuals.el
+++ emacs.d/conf/lisp/003_visuals.el
@@ -1,13 +1,16 @@
 ;;; package --- Summary
 ;;; Commentary:
 ;;; Code:
-;; (load-theme 'tsdh-dark t)
+(if (window-system)
+    (load-theme 'tango-dark t)
+  (load-theme 'tsdh-dark t))
 ;; (load-theme 'wombat t)
 ;; (load-theme 'manoj-dark t)
-;; (load-theme 'deeper-blue t)
+;; (load-theme 'tsdh-light t)
+;; (load-theme 'misterioso t)
 ;; (load-theme 'misterioso t)
 ;; (load-theme 'tango t)
-(load-theme 'tango-dark t)
+;; (load-theme 'tango-dark t)
 ;; (color-theme-taylor)
 
 ;; paren-mode 対応する括弧を強調して表示する
@@ -16,15 +19,13 @@
 (show-paren-mode t)
 
 ;; parenのスタイル: expression は括弧内も強調表示
-;;(setq show-paren-style 'expression)
+(setq show-paren-style 'expression)
 ;; mixed だと画面内に収まらない時にカッコ内も表示する
-(setq show-paren-style 'mixed)
+;; (setq show-paren-style 'mixed)
 
-(set-face-background 'show-paren-match-face "DeepPink")
-;;(set-face-background 'show-paren-match-face nil)
-;;(set-face-underline-p 'show-paren-match-face "yellow")
-;;(set-face-underline-p 'show-paren-match-face t)
-;;(setq show-paren-match-face 'underline)
+(set-face-attribute 'show-paren-match nil
+                    :background "#444444" :foreground nil
+                    :underline nil :weight 'bold)
 
 (when (require 'rainbow-mode nil t)
   (add-hook 'css-mode-hook 'rainbow-mode)
@@ -35,6 +36,21 @@
   (add-hook 'web-mode-hook 'rainbow-mode))
 
 (when (require 'rainbow-delimiters nil t)
-  (rainbow-delimiters-mode t))
+  (rainbow-delimiters-mode t)
+  (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+  ;; https://yoo2080.wordpress.com/2013/12/21/small-rainbow-delimiters-tutorial/#sec-5-1
+  ;; 5.1. using stronger colors
+  (require 'cl-lib)
+  (require 'color)
+  (cl-loop
+   for index from 1 to rainbow-delimiters-max-face-count
+   do
+   (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+     (cl-callf color-saturate-name (face-foreground face) 30))))
 
+(set-cursor-color "Green")
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "#444444")
+(set-face-foreground 'hl-line nil)
+(set-face-foreground 'highlight nil)
 ;;;
